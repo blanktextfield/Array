aa23eeb230bd0da0702240e1d482cfdf7de74b7e
Opilane TTHK <opilane@tthk.ee>
2020-09-28 14:51:28 +0300

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] shoppinglist = { "monster", "pop corn", "chips", "cookies", "chicken nuggets" };
+
+
+            int arrayLenght = shoppinglist.Length;
+            Console.WriteLine($"You have {arrayLenght} items on your list.");
+
+            for (int i = 0; i < shoppinglist.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {shoppinglist[i]}");
+            }
+            Console.WriteLine($"The first item on your list is {shoppinglist[0]}");
+            Console.WriteLine($"The last item on your list is {shoppinglist[shoppinglist.Length-1]}");
+
+        }
+    }
+}
